80d7c40a513bee0b6bf508f7c7ea2c8357b30924
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-02-17 15:19:21 -0800

    add react-dom-router and initial routes

--- app/components/App.js
+++ app/components/App.js
@@ -1,17 +1,27 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
-var Home = require('./Home');
 var Header = require('./Header');
+var Home = require('./Home');
+var Forecast = require('./Forecast');
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Home />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/forecast" component={Forecast} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 module.exports = App;
---
+++ app/components/Forecast.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+function Forecast() {
+  return (
+    <div>
+      <h1>Forecast component</h1>
+    </div>
+  );
+}
+
+module.exports = Forecast;
