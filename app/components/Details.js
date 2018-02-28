eb6103fc0ee438d8d70af0203a9187daaf367cbf
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-02-27 21:19:47 -0800

    add kelvin to fahrenheit helper

--- app/components/Details.js
+++ app/components/Details.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var getFormattedDate = require('../utils/helpers').getFormattedDate;
+var helpers = require('../utils/helpers');
 
 class Details extends React.Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class Details extends React.Component {
 
     const city = this.props.match.params.city;
     const icon = state.weather[0].icon;
-    const date = getFormattedDate(state.dt);
+    const date = helpers.getFormattedDate(state.dt);
     const description = state.weather[0].description;
     const min_temp = state.temp.min;
     const max_temp = state.temp.max;
@@ -29,8 +29,8 @@ class Details extends React.Component {
         <div className="details-bottom">
           <p>{city}</p>
           <p>{description}</p>
-          <p>min temp: {min_temp}</p>
-          <p>max temp: {max_temp}</p>
+          <p>min temp: {helpers.convertKelvinToFahrenheit(min_temp)}</p>
+          <p>max temp: {helpers.convertKelvinToFahrenheit(max_temp)}</p>
           <p>humidity: {humidity}</p>
         </div>
       </div>
--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,32 +1,40 @@
 const weekday = {
-    '0': 'Sunday',
-    '1': 'Monday',
-    '2': 'Tuesday',
-    '3': 'Wednesday',
-    '4': 'Thursday',
-    '5': 'Frieday',
-    '6': 'Saturday',
-}
+  '0': 'Sunday',
+  '1': 'Monday',
+  '2': 'Tuesday',
+  '3': 'Wednesday',
+  '4': 'Thursday',
+  '5': 'Frieday',
+  '6': 'Saturday'
+};
 
 const month = {
-    '0': 'Jan',
-    '1': 'Feb',
-    '2': 'Mar',
-    '3': 'Apr',
-    '4': 'May',
-    '5': 'Jun',
-    '6': 'Jul',
-    '0': 'Aug',
-    '1': 'Sep',
-    '2': 'Oct',
-    '3': 'Nov',
-    '4': 'Dec',
-}
+  '0': 'Jan',
+  '1': 'Feb',
+  '2': 'Mar',
+  '3': 'Apr',
+  '4': 'May',
+  '5': 'Jun',
+  '6': 'Jul',
+  '0': 'Aug',
+  '1': 'Sep',
+  '2': 'Oct',
+  '3': 'Nov',
+  '4': 'Dec'
+};
 
 module.exports = {
-    getFormattedDate(dt) {
-        let output = '';
-        const date = new Date(dt*1000);
-        return output += weekday[date.getDay()] + ', ' + month[date.getMonth()] + ' ' + date.getDate();
-    }
-}
+  getFormattedDate(dt) {
+    let output = '';
+    const date = new Date(dt * 1000);
+    return (output +=
+      weekday[date.getDay()] +
+      ', ' +
+      month[date.getMonth()] +
+      ' ' +
+      date.getDate());
+  },
+  convertKelvinToFahrenheit(k) {
+    return parseInt(k * (9 / 5) - 459.67) + ' degrees';
+  }
+};
