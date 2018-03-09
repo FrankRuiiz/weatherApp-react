87b9b4f695ebb2f7d916e79b22c0d68dcdc3581b
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-03-08 20:46:33 -0800

    refactor api.js to es6

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,29 +1,18 @@
 var axios = require('axios');
 
-var API_KEY = '4f3b63ed27461e8bbf8c08f24bb45840';
+const API_KEY = '4f3b63ed27461e8bbf8c08f24bb45840';
 
 module.exports = {
-  fetchCurrentWeather: function(location) {
-    var encodedURI = window.encodeURI(
-      'http://api.openweathermap.org/data/2.5/weather?q=' +
-        location +
-        '&type=accurate&APPID=' +
-        API_KEY
+  fetchCurrentWeather: location => {
+    const encodedURI = window.encodeURI(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=${API_KEY}`
     );
-    return axios.get(encodedURI).then(function(response) {
-      return response.data;
-    });
+    return axios.get(encodedURI).then(({ data }) => data);
   },
-  fetchFiveDayWeather: function(location) {
+  fetchFiveDayWeather: location => {
     var encodedURI = window.encodeURI(
-      'http://api.openweathermap.org/data/2.5/forecast/daily?q=' +
-        location +
-        '&type=accurate&APPID=' +
-        API_KEY +
-        '&cnt=5'
+      `http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&type=accurate&APPID=${API_KEY}&cnt=5`
     );
-    return axios.get(encodedURI).then(function(response) {
-      return response.data;
-    });
+    return axios.get(encodedURI).then(({ data }) => data);
   }
 };
